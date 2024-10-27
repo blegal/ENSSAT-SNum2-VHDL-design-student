d70b9a3d68c174a8dfce1e2fd44d517e504836a4
Bertrand LE GAL <bertrand.legal@ims-bordeaux.fr>
2024-10-27 17:20:44 +0100

    First commit

---
+++ design_compiler/dc-1-xor-gate-rtl.tcl
@@ -0,0 +1,28 @@
+rm -rf  BODY ARCH dc ENTI obj PACK *.syn *.mr
+
+set target_library [list /usr/local/DesignKit/hcmos9gp_920/CORE9GPLL_SNPS_AVT_4.1/SNPS/common/SYMBOL/CORE9GPLL.sdb	]
+set link_library   [list /usr/local/DesignKit/hcmos9gp_920/CORE9GPLL_SNPS_AVT_4.1/SNPS/common/SYMBOL/CORE9GPLL.sdb	]
+
+set hcmos9gp /usr/local/DesignKit/hcmos9gp_920/CORE9GPLL_SNPS_AVT_4.1/SNPS/bc_1.32V_0C_wc_1.08V_105C/PHS/CORE9GPLL_Nom.db 
+set search_path    "$search_path"
+set target_library "$hcmos9gp"
+set link_library   "* $hcmos9gp"
+set symbol_library "/usr/local/DesignKit/hcmos9gp_920/CORE9GPLL_SNPS_AVT_4.1/SNPS/common/SYMBOL/CORE9GPLL.sdb"
+
+echo "\n\nSettings:"
+echo "search_path:       $search_path"
+echo "link_library:      $link_library"
+echo "target_library:    $target_library"
+echo "symbol_library:    $symbol_library"
+
+analyze -library WORK -format vhdl { ../1-xor-gate-comb/xor_gate_comb.vhd }
+
+#
+#	COMPILATION NORMALE
+#
+
+elaborate xor_gate_comb -architecture ARCH
+
+#
+#
+#	COMPILATION ULTRA
