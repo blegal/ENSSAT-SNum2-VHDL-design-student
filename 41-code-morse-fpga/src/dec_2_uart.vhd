a2ff45df3c1016e36698818565a02473c233d6bc
Bertrand LE GAL <bertrand.legal@ims-bordeaux.fr>
2024-11-29 17:34:13 +0100

    Update dec_2_uart.vhd

--- 41-code-morse-fpga/src/dec_2_uart.vhd
+++ 41-code-morse-fpga/src/dec_2_uart.vhd
@@ -17,10 +17,20 @@ END dec_2_uart;
 ARCHITECTURE Behavioral OF dec_2_uart IS
 BEGIN
 
-    --
-    -- ... ... ... ... ... ...
-    -- ... ... ... ... ... ...
-    -- ... ... ... ... ... ...
-    --
+	PROCESS(RST, CLK)
+	BEGIN
+		IF RST = '1' THEN
+			DATA_O    <= x"00";
+			DATA_O_EN <=   '0';
+		ELSIF CLK = '1' AND CLK'EVENT THEN
+			IF DATA_I_EN = '1' AND CEN = '1' THEN
+				DATA_O    <= STD_LOGIC_VECTOR( UNSIGNED(DATA_I) + TO_UNSIGNED(65, 8));
+				DATA_O_EN <=   '1';
+			ELSE
+				DATA_O    <= x"00";
+				DATA_O_EN <=   '0';
+			END IF;
+		END IF;
+	END PROCESS;
 
 END Behavioral;
