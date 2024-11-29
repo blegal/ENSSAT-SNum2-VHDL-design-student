8c5f52dbafd9304f9840b64a2a90c68597a0bc16
Bertrand LE GAL <bertrand.legal@ims-bordeaux.fr>
2024-11-29 17:34:10 +0100

    Update uart_2_enc.vhd

--- 41-code-morse-fpga/src/uart_2_enc.vhd
+++ 41-code-morse-fpga/src/uart_2_enc.vhd
@@ -17,10 +17,20 @@ END uart_2_enc;
 ARCHITECTURE Behavioral OF uart_2_enc IS
 BEGIN
 
-    --
-    -- ... ... ... ... ... ...
-    -- ... ... ... ... ... ...
-    -- ... ... ... ... ... ...
-    --
+	PROCESS(RST, CLK)
+	BEGIN
+		IF RST = '1' THEN
+			DATA_O    <= x"00";
+			DATA_O_EN <=   '0';
+		ELSIF CLK = '1' AND CLK'EVENT THEN
+			IF DATA_I_EN = '1' THEN
+				DATA_O    <= STD_LOGIC_VECTOR( UNSIGNED(DATA_I) - TO_UNSIGNED(65, 8));
+				DATA_O_EN <=   '1';
+			ELSIF CEN = '1' THEN
+				DATA_O    <= x"00";
+				DATA_O_EN <=   '0';
+			END IF;
+		END IF;
+	END PROCESS;
 
 END Behavioral;
